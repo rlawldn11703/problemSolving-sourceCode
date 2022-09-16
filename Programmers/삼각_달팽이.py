c88733159dd91ffc7d482a8399b22b6888bcb352
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2022-09-16 15:57:46 +0900

    Update 삼각_달팽이.py

--- Programmers/삼각_달팽이.py
+++ Programmers/삼각_달팽이.py
@@ -3,17 +3,17 @@ def solution(n):
     idx,num,h = 0,1,0
     for i in range(n,0,-1):
         j = n-i
-        if j%3 == 0:
+        if j%3 == 0: # 아래로 증가
             for _ in range(i):
                 idx += h
                 ans[idx] = num
                 num, h = num+1 , h+1
-        elif j%3 == 1:
+        elif j%3 == 1: # 옆으로 증가
             for _ in range(i):
                 idx += 1
                 ans[idx] = num
                 num += 1
-        else:
+        else: # 위로 증가
             for _ in range(i):
                 idx -= h
                 ans[idx] = num
