b72e637699320a1e4bf5d71a437d393fd9e4eb78
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2022-12-14 15:52:10 +0900

    Create 커리큘럼.py

---
+++ 이것이_코딩_테스트다/Graph/커리큘럼.py
@@ -0,0 +1,23 @@
+from collections import deque
+import copy
+N = int(input())
+indegree,times = [[0,[]] for _ in range(N+1)],[0]*(N+1)
+ans = copy.deepcopy(times)
+for i in range(1,N+1):
+    info = list(map(int,input().split()))
+    times[i] = info[0]
+    indegree[i][0] = len(info[1:-1])
+    for j in info[1:-1]:
+        indegree[j][1].append(i)
+q = deque([])
+for i in range(1,N+1):
+    if indegree[i][0] == 0:
+        q.append(i)
+while q:
+    x = q.popleft()
+    for i in indegree[x][1]:
+        ans[i] = max(ans[i],ans[x]+times[i])
+        indegree[i][0] -= 1
+        if indegree[i][0]==0:
+            q.append(i)
+[print(ans[i]) for i in range(1,N+1)]
