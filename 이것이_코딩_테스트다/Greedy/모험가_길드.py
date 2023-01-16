d77063c207c91aa5bb276d5cffadc4b320617f23
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2023-01-16 13:21:17 +0900

    Update 모험가_길드.py

--- 이것이_코딩_테스트다/Greedy/모험가_길드.py
+++ 이것이_코딩_테스트다/Greedy/모험가_길드.py
@@ -1,12 +1,11 @@
 N = int(input())
 fear = list(map(int,input().split()))
 fear.sort()
-print(fear)
 # 공포도가 적은 모험가부터 데려가야 최대한 많은 그룹
-ans,i = 0,0
-while i < N:
-    i += max(fear[i:i+fear[i]])
-    if i>N:
-        continue
-    ans += 1
+ans,cnt = 0,0
+for f in fear:
+    cnt += 1
+    if cnt >= f:
+        ans += 1
+        cnt = 0
 print(ans)
