c58436aa618157dce6c37b15db28ae842856c856
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2023-01-25 14:55:16 +0900

    Update 연구소.py

--- 이것이_코딩_테스트다/DFS&BFS/연구소.py
+++ 이것이_코딩_테스트다/DFS&BFS/연구소.py
@@ -1,42 +1,44 @@
-from itertools import combinations
-from collections import deque
-from copy import deepcopy
-def bfs(B,virus,n,m):
+n,m = map(int,input().split())
+data,temp = [],[[0]*m for _ in range(n)]
+for _ in range(n):
+    data.append((list(map(int,input().split()))))
+result = 0
+def virus(x,y): #dfs이용해 각 바이러스가 사방으로 퍼지도록 하기
     dx,dy = [1,0,-1,0],[0,1,0,-1]
-    visited = [[False]*m for _ in range(n)]
-    Board,q,cnt = deepcopy(B),deque([]),0
-    for i,j in virus:
-        q.append((i,j))
-        visited[i][j] = True
-        while q:
-            x,y = q.popleft()
-            for w in range(4):
-                xx,yy = x+dx[w],y+dy[w]
-                if xx<0 or xx>=n or yy<0 or yy>=m:
-                    continue
-                if not visited[xx][yy] and Board[xx][yy]=='0':
-                    Board[xx][yy] = '2'
-                    visited[xx][yy] = True
-                    q.append((xx,yy))
+    for i in range(4):
+        nx,ny = x+dx[i],y+dy[i]
+        if 0<=nx<n and 0<=ny<m:
+            if temp[nx][ny]==0:
+                temp[nx][ny]=2
+                virus(nx,ny)
+def get_score():
+    score = 0
     for i in range(n):
         for j in range(m):
-            if Board[i][j]=='0':
-                cnt += 1
-    return cnt
-n,m = map(int,input().split())
-Board,empty,virus = [],[],[]
-for i in range(n):
-    Board.append(input().split())
-    for j in range(m):
-        if Board[i][j]=='0':
-            empty.append((i,j))
-        if Board[i][j]=='2':
-            virus.append((i,j))
-ans = -1
-for loc in combinations(empty,3):
-    for x,y in loc:
-        Board[x][y] = '1'
-    ans = max(ans,bfs(Board,virus,n,m))
-    for x,y in loc:
-        Board[x][y] = '0'
-print(ans)
+            if temp[i][j]==0:
+                score += 1
+    return score
+def dfs(count):
+    global result
+    if count==3: #울타리가 3개 설치된 경우
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j]==2:
+                    virus(i,j)
+        result = max(result,get_score())
+        return
+    for i in range(n): #빈 공간에 울타리 설치
+        for j in range(m):
+            if data[i][j]==0:
+                data[i][j]=1
+                count += 1
+                dfs(count)
+                data[i][j]=0
+                count -= 1
+dfs(0)
+print(result)
+#나동빈 강사님 답안
+#python3로 제출시,시간초과
