4988146e76632dae6962120f2a4a58f43c6837c8
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2023-01-25 15:46:11 +0900

    Create 2110.py

---
+++ BOJ/2110.py
@@ -0,0 +1,24 @@
+n,c = map(int,input().split())
+home = []
+for _ in range(n):
+    home.append(int(input()))
+home.sort()
+def dist(num,c):
+    idx,cnt = 0,1
+    for i in range(1,len(home)):
+        if home[i]-home[idx]>=num:
+            cnt += 1
+            idx = i
+    if cnt >= c:
+        return True
+    else:
+        return False
+left,right,result = 0,home[-1]-home[0],0
+while left<=right:
+    mid = (left+right)//2
+    if dist(mid,c):
+        left = mid+1
+        result = mid
+    else:
+        right = mid-1
+print(result)
