1c12b703dc71d5d9253dfbae6d1b3d1b933d48ea
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2023-02-02 17:59:38 +0900

    Create 경쟁적_전염.py

---
+++ 이것이_코딩_테스트다/DFS&BFS/경쟁적_전염.py
@@ -0,0 +1,24 @@
+from collections import deque
+n,k = map(int,input().split())
+graph,data = [],[]
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+    for j in range(n):
+        if graph[i][j] != 0:
+            data.append((graph[i][j],0,i,j))
+data.sort()
+q = deque(data)
+t_s,t_x,t_y = map(int,input().split())
+dx,dy = [-1,0,1,0],[0,1,0,-1]
+while q:
+    virus,s,x,y = q.popleft()
+    if s==t_s:
+        break
+    for i in range(4):
+        nx,ny = x+dx[i],y+dy[i]
+        if 0<=nx<n and 0<=ny<n:
+            if graph[nx][ny]==0:
+                graph[nx][ny] = virus
+                q.append((virus,s+1,nx,ny))
+print(graph[t_x-1][t_y-1])
+        
