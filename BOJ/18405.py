501b25a9c2d3d0855890eaca44cc94f7b7a5b61f
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2023-02-02 18:00:20 +0900

    Update 18405.py

--- BOJ/18405.py
+++ BOJ/18405.py
@@ -6,7 +6,7 @@ for i in range(n):
     for j in range(n):
         if test[i][j] > 0:
             virus.append((i,j))
-round = [[-1]*n for _ in range(n)]
+rnd = [[-1]*n for _ in range(n)]
 s,i,j = map(int,input().split())
 dx,dy = [1,0,-1,0],[0,1,0,-1]
 for r in range(s):
@@ -20,9 +20,9 @@ for r in range(s):
                 continue
             if test[nx][ny]==0: #비어있다면
                 test[nx][ny]=test[x][y] #전염시키기
-                round[nx][ny] = r
+                rnd[nx][ny] = r
                 virus.append((nx,ny))
-            elif round[nx][ny]==r: #바이러스가 존재하면서 해당 초에 감염된 것이라면
+            elif rnd[nx][ny]==r: #바이러스가 존재하면서 해당 초에 감염된 것이라면
                 test[nx][ny] = min(test[nx][ny],test[x][y])
                 virus.append((nx,ny))
 print(test[i-1][j-1])
