8511c7cbf6abe72096c3535e42aebb97fe582a17
rlawldn11703 <71757471+rlawldn11703@users.noreply.github.com>
2023-02-02 17:39:55 +0900

    Create 18405.py

---
+++ BOJ/18405.py
@@ -0,0 +1,28 @@
+from collections import deque
+n,k = map(int,input().split())
+test,virus = [],deque([])
+for i in range(n):
+    test.append(list(map(int,input().split())))
+    for j in range(n):
+        if test[i][j] > 0:
+            virus.append((i,j))
+round = [[-1]*n for _ in range(n)]
+s,i,j = map(int,input().split())
+dx,dy = [1,0,-1,0],[0,1,0,-1]
+for r in range(s):
+    v = len(virus)
+    while v>0:
+        x,y = virus.popleft()
+        v -= 1
+        for w in range(4):
+            nx,ny = x+dx[w],y+dy[w]
+            if nx<0 or nx>=n or ny<0 or ny>=n:
+                continue
+            if test[nx][ny]==0: #비어있다면
+                test[nx][ny]=test[x][y] #전염시키기
+                round[nx][ny] = r
+                virus.append((nx,ny))
+            elif round[nx][ny]==r: #바이러스가 존재하면서 해당 초에 감염된 것이라면
+                test[nx][ny] = min(test[nx][ny],test[x][y])
+                virus.append((nx,ny))
+print(test[i-1][j-1])
