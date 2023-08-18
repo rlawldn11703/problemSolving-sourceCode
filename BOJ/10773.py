8c2451953abfcbfa030078e713e74e2b8d87543e
Jiwoo Kim <71757471+rlawldn11703@users.noreply.github.com>
2023-08-18 13:30:31 +0900

    Create 10773.py

---
+++ BOJ/10773.py
@@ -0,0 +1,11 @@
+import sys
+k = int(sys.stdin.readline())
+stack = []
+for _ in range(k):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+ans = sum(stack)
+print(ans)
